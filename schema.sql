7deacede0c30bf8ab028a5183b0072a7a5b071ab
dialite <raymondolisa01@gmail.com>
2023-01-18 16:12:09 +0100

    Add a column 'species' to schema.sql and insert data to data.sql

--- schema.sql
+++ schema.sql
@@ -6,5 +6,7 @@ CREATE TABLE animals (
     date_of_birth date,
     escape_attempts INT,
     neutered BOOLEAN,
-    weight_kg DECIMAL
+    weight_kg DECIMAL,
+    species VARCHAR;
 );
+ALTER TABLE animals ADD COLUMN species VARCHAR(100);
