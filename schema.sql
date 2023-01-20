25fe8ccf152e1659450d22ba9f0035b957754d1e
dialite <raymondolisa01@gmail.com>
2023-01-20 22:53:25 +0100

    Create vets, specializations and visits table in schemal.sql

--- schema.sql
+++ schema.sql
@@ -37,3 +37,25 @@ ADD CONSTRAINT fk_species FOREIGN KEY(species_id) REFERENCES species(id);
 --Add column owner_id which is a foreign key referencing the owners table
 ALTER TABLE animals ADD owner_id INT,
 ADD CONSTRAINT fk_owners FOREIGN KEY(owner_id) REFERENCES owners(id);
+
+CREATE TABLE vets (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(150) NOT NULL,
+  age INTEGER,
+  date_of_graduation DATE
+);
+
+-- Species, vet many to many relationship
+CREATE TABLE specializations (
+    species_id INTEGER REFERENCES species (id),
+    vet_id INTEGER REFERENCES vets (id),
+    PRIMARY KEY (species_id, vet_id)
+);
+
+-- Animal, vet many to many relationship
+CREATE TABLE visits (
+    animal_id INTEGER REFERENCES animals (id),
+    vet_id INTEGER REFERENCES vets (id),
+    visit_date TIMESTAMP NOT NULL,
+    PRIMARY KEY (animal_id, vet_id, visit_date)
+);
